687b53834876d33dd993604503ff43f0eaed7501
D1JS <d1js@TECHIN.INTERNAL>
2024-12-06 16:13:45 +0200

    Navbar with response

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,17 +8,21 @@ import bookmarked from "../assets/icon-nav-bookmark.svg"
 
 const NavBar = () => {
   return (
-    <div className="w-full flex flex-cols-3 justify-between p-4 h-[56px] bg-dark items-center">
-    <img className="w-[25px] h-[20px]" src={logo} alt="" />
-    <div className="navbar-center flex flex-cols-4 w-[133.54px] items-center justify-between">
-    <img className="w-[16px] h-[16px]" src={home} alt="home" />
-    <img className="w-[16px] h-[16px]" src={movies} alt="movies" />
-    <img className="w-[16px] h-[16px]" src={tv} alt="tv" />
-    <img className="w-[16px] h-[16px]" src={bookmarked} alt="bookmarked" />
+    <div className="box-content">
+    <div className=" grid grid-cols-3 p-4 h-[56px] bg-dark  items-center md:rounded-[0.625rem] md:m-4 lg:grid-cols-1 lg:float-left lg:h-[60rem] lg:justify-items-center lg:grid-rows-[20px_1fr_24px] lg:gap-12">
+    <img className="w-[25px] h-[20px] md:w-[32px] md:h-[25.6px]" src={logo} alt="" />
+    <div className="navbar-center flex flex-cols-4 w-[133.54px] md:w-[172.92px] items-center justify-between lg:flex-col lg:h-[200px] lg:w-[3rem] place-self-center lg:place-self-start ">
+    <img className="w-[16px] h-[16px] md:w-[20px] md:h-[20px]" src={home} alt="home" />
+    <img className="w-[16px] h-[16px] md:w-[20px] md:h-[20px]" src={movies} alt="movies" />
+    <img className="w-[16px] h-[16px] md:w-[20px] md:h-[20px]" src={tv} alt="tv" />
+    <img className="w-[13.5px] h-[16px] md:w-[17.5px] md:h-[20px]" src={bookmarked} alt="bookmarked" />
     </div>
-    <img className="w-[24px] h-[24px]" src={avatar} alt="" />
+    <img className="w-[24px] h-[24px] md:w-[32px] md:h-[32px] place-self-center" src={avatar} alt="" />
+    </div>
     </div>
   );
 };
+
+
 
 export default NavBar;
