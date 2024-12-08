99c285dda614653e9dd39d51297554a9fd27e1fe
Paul <povilas.geguzis@gmail.com>
2024-12-08 13:27:12 +0200

    fixed another bug where bookmark button would transition twice

--- src/components/Bookmark.jsx
+++ src/components/Bookmark.jsx
@@ -15,7 +15,7 @@ function Bookmark({ film, setUpdate }) {
 
   const bookmarkButton = isBookmarked ? bookmarkFull : bookmarkEmpty;
   return (
-    <div className="bookmark-button hover:circle-icon-hover">
+    <div className="bookmark-button">
       <button onClick={bookmarkHandler} className="circle-icon">
         <img src={bookmarkButton} alt={bookmarkButton} />
       </button>
