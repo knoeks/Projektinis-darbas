a929c31111f6375441fff3e78a2e3af549e844cb
Andrej Titkov <ronya.titkov@gmail.com>
2024-12-09 22:27:30 +0200

    Experimenting with Outlet and Context

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import HomePage from "./components/HomePage";
 import MoviePage from "./components/MoviePage";
 import SeriesPage from "./components/SeriesPage";
 import BookmarkedPage from "./components/BookmarkedPage";
-import { Route, Routes } from "react-router";
+import { Route, Routes, Outlet } from "react-router";
 import NotFound from "./components/NotFound";
 import { useEffect, useState } from "react";
 import { getAll } from "./helpers/get";
@@ -11,6 +11,8 @@ function App() {
   const [update, setUpdate] = useState(0);
   const [error, setError] = useState("");
   const [allFilms, setAllFilms] = useState([]);
+  const [filteredFilms, setFilteredFilms] = useState([]);
+  const [category, setCategory] = useState("");
 
   const fetchData = async () => {
     try {
@@ -24,32 +26,36 @@ function App() {
   useEffect(() => {
     fetchData();
   }, [update]);
+
   return (
     <>
       <Routes>
-        <Route path="/home" element={<HomePage setUpdate={setUpdate} allFilms={allFilms}/>} />
-        <Route path="/movies" element={<MoviePage setUpdate={setUpdate} allFilms={allFilms}/>} />
-        <Route path="/series" element={<SeriesPage setUpdate={setUpdate} allFilms={allFilms}/>} />
-        <Route path="/bookmarked" element={<BookmarkedPage setUpdate={setUpdate} allFilms={allFilms}/>} />
-        <Route path="/*" element={<NotFound />} />
+        <Route
+          path="/"
+          element={
+            <LayoutContext
+              context={{ setUpdate, allFilms, filteredFilms, setFilteredFilms, category, setCategory }}
+            />
+          }
+        >
+          <Route path="home" element={<HomePage />} />
+          <Route path="movies" element={<MoviePage />} />
+          <Route path="series" element={<SeriesPage />} />
+          <Route path="bookmarked" element={<BookmarkedPage />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
       </Routes>
       {error && <p>{error}</p>}
     </>
   );
 }
 
+function LayoutContext({ context }) {
+  return (
+    <div>
+      <Outlet context={context} />
+    </div>
+  );
+}
+
 export default App;
-
-//   const [filteredFilms, setFilteredFilms] = useState([]);
-//   const [category, setCategory] = useState("");
-
-//   return (
-//     <>
-//       <SearchBar itemArray={allFilms} onFilter={setFilteredFilms}/>
-//       {error ? (
-//         <p>{error}</p>
-//       ) : (
-//         <Films category={category} filteredFilms={filteredFilms} setUpdate={setUpdate} />
-//       )}
-//     </>
-//   );
