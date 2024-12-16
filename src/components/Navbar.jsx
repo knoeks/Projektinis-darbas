06bed02a64db62c6f6186c55624bad0209f790e6
Paul <povilas.geguzis@gmail.com>
2024-12-16 16:15:17 +0200

    moved some style to index.css

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,7 +12,7 @@ const NavBar = () => {
 
   return (
     <div className="md:px-8 md:pt-8 md:pb-2 xl:pr-0 box-border fixed top-0 left-0 z-50 w-full bg-light xl:h-full xl:w-fit">
-      <div className="nav-container w-full xl:w-24">
+      <div className="nav-container">
         <Logo className="logo" />
         <div className="nav-2nd-container">
           <Link
