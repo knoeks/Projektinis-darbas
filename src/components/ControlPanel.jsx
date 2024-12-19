a1fd8638a84295561b1e932c79de47a347d0569c
Paul <povilas.geguzis@gmail.com>
2024-12-19 10:18:36 +0200

    fixed style

--- src/components/ControlPanel.jsx
+++ src/components/ControlPanel.jsx
@@ -3,7 +3,7 @@ import DeleteButton from "./DeleteButton"
 function ControlPanel({ film }) {
   return (
 
-      <div className="flex flex-col justify-between items-center absolute right-0 p-2 -z-0 h-full w-16 bg-dark bg-opacity-75">
+      <div className="flex flex-col justify-between items-center absolute left-0 p-2 -z-0 h-[90%] top-1/2 bottom-1/2 -translate-y-1/2 ml-2 md:ml-2 w-12 bg-dark bg-opacity-75 rounded-full">
       </div>
 
   )
