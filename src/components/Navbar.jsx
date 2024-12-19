5d413134f6b9e0c2f6bb476b384ade3940ccceaf
Paul <povilas.geguzis@gmail.com>
2024-12-19 12:25:04 +0200

    fixed visual bugs

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,7 +13,7 @@ const NavBar = ({role}) => {
 
 
   return (
-      <div className="md:px-8 md:pt-8 md:pb-2 xl:pr-0 box-border sticky xl:fixed top-0 left-0 z-50 w-full bg-light xl:h-full xl:w-fit">
+      <div className="md:px-8 md:pt-8 md:pb-2 xl:pr-0 xl:pb-[2.8125rem] box-border sticky xl:fixed top-0 left-0 z-50 w-full bg-light xl:h-full xl:w-fit">
         <div className="nav-container">
           <Logo className="logo" />
           <div className="nav-2nd-container">
